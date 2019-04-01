b8907130084ab75e5553659e14f31229506ec574
gma712 <yuhei_kajima@nuco.co.jp>
2019-04-02 00:50:44 +0900

    add affin layer and softmax layer

--- common/layers.py
+++ common/layers.py
@@ -1,6 +1,8 @@
 #coding: utf-8
 
 import numpy as np
+from common import functions
+from common import loss_functions
 
 class MulLayer:
   def __init__(self):
@@ -68,4 +70,42 @@ class Sigmoid:
 
     return dx
 
+class Affine:
+  def __init__(self, W, b):
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dw = None
+    self.db = None
 
+  def forward(self, x):
+    self.x = x
+    out = np.dot(x, self.W) + self.b
+
+    return out
+
+  def backward(self, dout):
+    dx = np.dot(dout, self.W.T)
+    self.dW = np.dot(self.x.T, dout)
+    self.db = np.sum(dout, axis=0)
+
+    return dx
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+
+  def forward(self, x, t):
+    self.t = t
+    self.y = functions.softmax(x)
+    self.loss = loss_functions.cross_entropy_error(self.y, self.t)
+    
+    return self.loss
+
+  def backward(self, dout=1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
+
+    return dx
