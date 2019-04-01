a40c2dfaa97bec7a90c5da68892de07e84f9418d
gma712 <yuhei_kajima@nuco.co.jp>
2019-04-01 23:42:51 +0900

    first commit

---
+++ common/functions.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def plt_sig(x):
+    y = sigmoid(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
